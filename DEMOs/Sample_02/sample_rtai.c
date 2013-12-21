e14afbf1244be3d35644849688f116209aeca3cc
nosnilwar <nosnilwar@gmail.com>
2013-12-21 15:07:03 -0400

    Acrescentado novos exemplos.

---
+++ DEMOs/Sample_02/sample_rtai.c
@@ -0,0 +1,16 @@
+// globals.h file -- Contains the global declarations and necessary header includes.
+
+#include <rtai_lxrt.h>
+#include <pthread.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int hard_timer_running = 1;
+	static RTIME sampling_interval;
+
+	printf("HELLO WORLD - TESTANDO INCLUDES...\n");
+
+	exit(1);
+	return 0;
+}
