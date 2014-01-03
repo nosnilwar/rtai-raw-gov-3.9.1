4f54eaa6f269fa5b3bf648ccbec0eede5e948ade
nosnilwar <nosnilwar@gmail.com>
2014-01-03 17:42:30 -0400

    Melhorado exemplo 2 da pasta DEMOs

--- DEMOs/Sample_02/led.c
+++ DEMOs/Sample_02/led.c
@@ -8,7 +8,8 @@ static int ioport = 0x378;
 module_param(ioport, int, 0664);
 MODULE_PARM_DESC(ioport, "Porta para I/O");
 
-static RT_TASK blink_task;
+static RT_TASK blink_task_1;
+static RT_TASK blink_task_2;
 
 static void blink_thread(long port)
 {
@@ -26,19 +27,27 @@ static void blink_thread(long port)
 
 int init_module(void)
 {
+	int cpuid1 = 0; // processador ID = 0
+	int cpuid2 = 1; // processador ID = 1
 	RTIME tick;
 
 	rt_set_oneshot_mode(); // Passa a ter com referencia os ciclos de frequencia do CPU e nao o tempo...
 	tick = start_rt_timer(nano2count(DESIRED_TICK));
-	rt_task_init(&blink_task, blink_thread, ioport, 2048, 1, 0, NULL);
-	rt_task_make_periodic(&blink_task, rt_get_time(), 1000*tick);
-	return 0;
+
+	//TODO:RAWLINSON rt_task_init(&blink_task_1, blink_thread, ioport, 2048, 1, 0, NULL);
+	rt_task_init_cpuid(&blink_task_1, blink_thread, ioport, 2048, 1, 0, NULL, cpuid1);
+	rt_task_init_cpuid(&blink_task_2, blink_thread, ioport, 2048, 1, 0, NULL, cpuid2);
+
+	rt_task_make_periodic(&blink_task_1, rt_get_time(), 1000*tick);
+	rt_task_make_periodic(&blink_task_2, rt_get_time(), 10000*tick);
+		return 0;
 }
 
 void cleanup_module(void)
 {
 	stop_rt_timer();
-	rt_task_delete(&blink_task);
+	rt_task_delete(&blink_task_1);
+	rt_task_delete(&blink_task_2);
 	outb(0, ioport);
 }
 
