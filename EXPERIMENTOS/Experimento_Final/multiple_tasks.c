c27cce06825489b7c9e5fe2e845afbc1683e02b0
nosnilwar <nosnilwar@gmail.com>
2014-06-05 11:57:52 -0400

    Adicionado o MatMult e Bsort ao escalonamento de tarefas do Raw Governor.

--- EXPERIMENTOS/Experimento_Final/multiple_tasks.c
+++ EXPERIMENTOS/Experimento_Final/multiple_tasks.c
@@ -50,9 +50,9 @@ char fundo_branco[8] = "\033[47m"; //Cor do fundo branca
 
 #define STACK_SIZE 2000
 
-#define NTASKS 1
+#define NTASKS 3
 
-char arrayTextoCorIdTask[NTASKS][8] = {"\033[31m"};//, "\033[32m"};//, "\033[34m", "\033[36m"}; // O texto no qual as tarefas serao imprimidas na tela.
+char arrayTextoCorIdTask[NTASKS][8] = {"\033[31m", "\033[32m", "\033[34m"};//, "\033[36m"}; // O texto no qual as tarefas serao imprimidas na tela.
 
 RT_TASK *arrayTasks[NTASKS];
 pthread_t *arrayThreads[NTASKS];
@@ -68,6 +68,7 @@ struct thread_param {
 /**************************************************
  * INICIO: DEFINICOES DO C-BENCHMARK -> CNT
  **************************************************/
+//#define WORSTCASE_CNT 1
 #define MAXSIZE 7000
 typedef int matrixCnt [MAXSIZE][MAXSIZE];
 
@@ -106,7 +107,7 @@ void SumCnt(matrixCnt Array)
 
   for (Outer = 0; Outer < MAXSIZE; Outer++) //Maxsize = 100
 	 for (Inner = 0; Inner < MAXSIZE; Inner++)
-#ifdef WORSTCASE
+#ifdef WORSTCASE_CNT
     	if (Array[Outer][Inner] >= 0) {
 #else
     	if (Array[Outer][Inner] < 0) {
@@ -219,6 +220,290 @@ void *init_task_cnt(void *arg)
  * FIM: DEFINICOES DO C-BENCHMARK -> CNT
  **************************************************/
 
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+/**************************************************
+ * INICIO: DEFINICOES DO C-BENCHMARK -> MatMult
+ *
+ * MATRIX MULTIPLICATION BENCHMARK PROGRAM:
+ * This program multiplies 2 square matrices resulting in a 3rd
+ * matrix. It tests a compiler's speed in handling multidimensional
+ * arrays and simple arithmetic.
+ **************************************************/
+#define UPPERLIMIT 650
+typedef int matrixMatMult [UPPERLIMIT][UPPERLIMIT];
+int SeedMatMult;
+matrixMatMult ArrayA, ArrayB, ResultArray;
+
+// Generates random integers between 0 and 8095
+int RandomIntegerMatMult(void)
+{
+   SeedMatMult = ((SeedMatMult * 133) + 81) % 8095;
+   return (SeedMatMult);
+}
+
+// Intializes the given array with random integers.
+void InitializeMatMult(matrixMatMult Array)
+{
+   int OuterIndex, InnerIndex;
+   for (OuterIndex = 0; OuterIndex < UPPERLIMIT; OuterIndex++)
+      for (InnerIndex = 0; InnerIndex < UPPERLIMIT; InnerIndex++)
+         Array[OuterIndex][InnerIndex] = RandomIntegerMatMult();
+}
+
+// Multiplies arrays A and B and stores the result in ResultArray.
+void MultiplyMatMult(matrixMatMult A, matrixMatMult B, matrixMatMult Res)
+{
+   register int Outer, Inner, Index;
+   for (Outer = 0; Outer < UPPERLIMIT; Outer++)
+      for (Inner = 0; Inner < UPPERLIMIT; Inner++)
+      {
+         Res [Outer][Inner] = 0;
+         for (Index = 0; Index < UPPERLIMIT; Index++)
+            Res[Outer][Inner]  +=
+               A[Outer][Index] * B[Index][Inner];
+       }
+}
+
+/*
+ * Runs a multiplication test on an array.  Calculates and prints the
+ * time it takes to multiply the matrices.
+ */
+void TestMatMult(matrixMatMult A, matrixMatMult B, matrixMatMult Res)
+{
+   InitializeMatMult(A);
+   InitializeMatMult(B);
+
+   MultiplyMatMult(A, B, Res);
+}
+
+// Initializes the seed used in the random number generator.
+void InitSeedMatMult(void)
+{
+  /* ***UPPSALA WCET***:
+     changed Thomas Ls code to something simpler.
+   SeedMatMult = KNOWN_VALUE - 1; */
+   SeedMatMult = 1;
+}
+
+void *init_task_matmult(void *arg)
+{
+	int idTask = ((struct thread_param*) arg)->idTask;
+	//RTIME taskDeadline = 0;
+	//RTIME tick_timer_atual = 0;
+	//RTIME tick_timer_restante = 0;
+
+	// Variaveis para realizar os calculos de tempo...
+	struct tm *newtime;
+	time_t aclock;
+
+	RTIME inicioExecucao = 0;
+	RTIME terminoExecucao = 0;
+	RTIME terminoPeriodo = 0;
+	RTIME Tperiodo;
+	RTIME Tinicio;
+	float tempo_processamento_tarefa;
+	float periodo_tarefa;
+	int prioridade = idTask + 1;
+
+	//double tempoProcessamento = 0.0;
+	long int WCEC = 9071928490; // cycles
+	unsigned int cpuFrequencyInicial = 1800000; // Hz
+	unsigned int cpuVoltageInicial = 5; // V
+
+	if(!(arrayTasks[idTask] = rt_task_init_schmod(idTask, prioridade, STACK_SIZE, 0, SCHED_FIFO, CPU_ALLOWED)))
+	{
+		printf("[ERRO] Não foi possível criar a tarefa MatMult (C-Benchmark).\n");
+		exit(1);
+	}
+
+	rt_allow_nonroot_hrt();
+
+	Tinicio = timeline_sched;
+	Tperiodo = tick_period * 161; // ~= 8 segundos (PERIODO == DEADLINE)
+
+	rt_change_prio(arrayTasks[idTask], prioridade);
+	rt_task_make_periodic(arrayTasks[idTask], Tinicio, Tperiodo);
+
+	printf("%s[TASK %d] Criada com Sucesso  =======> %llu\n", arrayTextoCorIdTask[idTask], idTask, Tperiodo);
+
+	while (1)
+	{
+		inicioExecucao = rt_get_cpu_time_ns(); //** PEGA O TEMPO DE INICIO DA EXECUCAO.
+
+		rt_cfg_init_info(arrayTasks[idTask], WCEC, cpuFrequencyInicial, cpuVoltageInicial); // Lugar correto...
+
+		/** INICIO: PROCESSANDO A TAREFA... **/
+		InitSeedMatMult();
+		TestMatMult(ArrayA, ArrayB, ResultArray);
+		/** FIM: PROCESSANDO A TAREFA... **/
+
+		/** CALCULANDO TEMPO DE PROCESSAMENTO DA TAREFA... **/
+		time(&aclock); // Pega tempo em segundos.
+		newtime = localtime(&aclock);
+		terminoExecucao = rt_get_cpu_time_ns(); //** PEGA O TEMPO DE FIM DA EXECUCAO.
+		tempo_processamento_tarefa = (terminoExecucao - inicioExecucao) / 1000000000.0; // Transformando de nanosegundo para segundo (10^9).
+
+		printf("%s[TASK %d] ##### Tempo processamento: %.10f => %s", arrayTextoCorIdTask[idTask], idTask, tempo_processamento_tarefa, asctime(newtime));
+
+		// Sinaliza para o RAW GOVERNOR que a tarefa concluio o seu processamento...
+		rt_cfg_set_rwcec(arrayTasks[idTask], 0);
+
+		rt_task_wait_period(); // **** WAIT
+
+		/** CALCULANDO TEMPO DE DURACAO DO PERIODO DA TAREFA... **/
+		time(&aclock); // Pega tempo em segundos.
+		newtime = localtime(&aclock);
+		terminoPeriodo = rt_get_cpu_time_ns();
+		periodo_tarefa = (terminoPeriodo - inicioExecucao) / 1000000000.0; // Transformando de nanosegundo para segundo (10^9).
+
+		printf("%s[TASK %d] ##### Duracao do Periodo   ==================================================> Duracao: %.10f => %s", arrayTextoCorIdTask[idTask], idTask, periodo_tarefa, asctime(newtime));
+		printf("%s", texto_preto);
+	}
+}
+/**************************************************
+ * FIM: DEFINICOES DO C-BENCHMARK -> MatMult
+ **************************************************/
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+/**************************************************
+ * INICIO: DEFINICOES DO C-BENCHMARK -> Bsort
+ **************************************************/
+
+#define KNOWN_VALUE (int) 2 /* A read from this address will result in an known value of 1 */
+#define UNKNOWN_VALUE (int) 2 /* A read from this address will result in an unknown value */
+#define WORSTCASE 1
+#define FALSE 0
+#define TRUE 1
+#define NUMELEMS 10000
+#define MAXDIM   (NUMELEMS+1)
+
+// Globals
+int ArrayBsort[MAXDIM], Seed;
+int factor;
+
+// Initializes given array with randomly generated integers.
+void InitializeBsort(int Array[MAXDIM])
+{
+	int  Index, fact;
+
+#ifdef WORSTCASE
+	factor = -1;
+#else
+	factor = 1;
+#endif
+
+	fact = factor;
+	for (Index = 1; Index <= NUMELEMS; Index ++)
+		Array[Index] = Index * fact * KNOWN_VALUE;
+}
+
+// Sorts an array of integers of size NUMELEMS in ascending order.
+void BubbleSort(int Array[MAXDIM])
+{
+   int Sorted = FALSE;
+   int Temp, /** LastIndex,**/ Index, i;
+   for (i = 1; i <= NUMELEMS-1; i++) /* apsim_loop 1 0 */
+   {
+      Sorted = TRUE;
+      for (Index = 1; Index <= NUMELEMS-1; Index ++) /* apsim_loop 10 1 */
+	  {
+         if (Index > NUMELEMS-i)
+            break;
+
+         if (Array[Index] > Array[Index + 1])
+         {
+            Temp = Array[Index];
+            Array[Index] = Array[Index+1];
+            Array[Index+1] = Temp;
+            Sorted = FALSE;
+         }
+      }
+      if (Sorted)
+         break;
+   }
+}
+
+void *init_task_bsort(void *arg)
+{
+	int idTask = ((struct thread_param*) arg)->idTask;
+	//RTIME taskDeadline = 0;
+	//RTIME tick_timer_atual = 0;
+	//RTIME tick_timer_restante = 0;
+
+	// Variaveis para realizar os calculos de tempo...
+	struct tm *newtime;
+	time_t aclock;
+
+	RTIME inicioExecucao = 0;
+	RTIME terminoExecucao = 0;
+	RTIME terminoPeriodo = 0;
+	RTIME Tperiodo;
+	RTIME Tinicio;
+	float tempo_processamento_tarefa;
+	float periodo_tarefa;
+	int prioridade = idTask + 1;
+
+	//double tempoProcessamento = 0.0;
+	long int WCEC = 6500290074; // cycles
+	unsigned int cpuFrequencyInicial = 1800000; // Hz
+	unsigned int cpuVoltageInicial = 5; // V
+
+	if(!(arrayTasks[idTask] = rt_task_init_schmod(idTask, prioridade, STACK_SIZE, 0, SCHED_FIFO, CPU_ALLOWED)))
+	{
+		printf("[ERRO] Não foi possível criar a tarefa Bsort (C-Benchmark).\n");
+		exit(1);
+	}
+
+	rt_allow_nonroot_hrt();
+
+	Tinicio = timeline_sched;
+	Tperiodo = tick_period * 241; // ~= 12 segundos (PERIODO == DEADLINE)
+
+	rt_change_prio(arrayTasks[idTask], prioridade);
+	rt_task_make_periodic(arrayTasks[idTask], Tinicio, Tperiodo);
+
+	printf("%s[TASK %d] Criada com Sucesso  =======> %llu\n", arrayTextoCorIdTask[idTask], idTask, Tperiodo);
+
+	while (1)
+	{
+		inicioExecucao = rt_get_cpu_time_ns(); //** PEGA O TEMPO DE INICIO DA EXECUCAO.
+
+		rt_cfg_init_info(arrayTasks[idTask], WCEC, cpuFrequencyInicial, cpuVoltageInicial); // Lugar correto...
+
+		/** INICIO: PROCESSANDO A TAREFA... **/
+		InitializeBsort(ArrayBsort);
+	    BubbleSort(ArrayBsort);
+		/** FIM: PROCESSANDO A TAREFA... **/
+
+		/** CALCULANDO TEMPO DE PROCESSAMENTO DA TAREFA... **/
+		time(&aclock); // Pega tempo em segundos.
+		newtime = localtime(&aclock);
+		terminoExecucao = rt_get_cpu_time_ns(); //** PEGA O TEMPO DE FIM DA EXECUCAO.
+		tempo_processamento_tarefa = (terminoExecucao - inicioExecucao) / 1000000000.0; // Transformando de nanosegundo para segundo (10^9).
+
+		printf("%s[TASK %d] ##### Tempo processamento: %.10f => %s", arrayTextoCorIdTask[idTask], idTask, tempo_processamento_tarefa, asctime(newtime));
+
+		// Sinaliza para o RAW GOVERNOR que a tarefa concluio o seu processamento...
+		rt_cfg_set_rwcec(arrayTasks[idTask], 0);
+
+		rt_task_wait_period(); // **** WAIT
+
+		/** CALCULANDO TEMPO DE DURACAO DO PERIODO DA TAREFA... **/
+		time(&aclock); // Pega tempo em segundos.
+		newtime = localtime(&aclock);
+		terminoPeriodo = rt_get_cpu_time_ns();
+		periodo_tarefa = (terminoPeriodo - inicioExecucao) / 1000000000.0; // Transformando de nanosegundo para segundo (10^9).
+
+		printf("%s[TASK %d] ##### Duracao do Periodo   ==================================================> Duracao: %.10f => %s", arrayTextoCorIdTask[idTask], idTask, periodo_tarefa, asctime(newtime));
+		printf("%s", texto_preto);
+	}
+}
+/**************************************************
+ * FIM: DEFINICOES DO C-BENCHMARK -> Bsort
+ **************************************************/
+
 int create_tasks(void)
 {
 	int i;
@@ -269,22 +554,42 @@ int create_tasks(void)
 	/*********************************************************
 	* Iniciando C-Benchmark -> MatMult
 	**********************************************************/
-//	if((arrayThreadParams[1] = malloc(sizeof(*arrayThreadParams[1]))) == NULL)
-//	{
-//		printf("[ERRO] Não foi possivel criar os parametros da tarefa MatMult (C-Benchmark).\n\n");
-//		return (-1);
-//	}
-//	else
-//	{
-//		arrayThreadParams[1]->idTask = 1;
-//
-//		// Inicializando as tarefas...
-//		if(pthread_create(arrayThreads[1], 0, init_task_matmult, (void *)arrayThreadParams[1]))
-//		{
-//			printf("[ERRO] Não foi possível inicializar a Thread da tarefa MatMult (C-Benchmark).\n");
-//			return(0);
-//		}
-//	}
+	if((arrayThreadParams[1] = malloc(sizeof(*arrayThreadParams[1]))) == NULL)
+	{
+		printf("[ERRO] Não foi possivel criar os parametros da tarefa MatMult (C-Benchmark).\n\n");
+		return (-1);
+	}
+	else
+	{
+		arrayThreadParams[1]->idTask = 1;
+
+		// Inicializando as tarefas...
+		if(pthread_create(arrayThreads[1], 0, init_task_matmult, (void *)arrayThreadParams[1]))
+		{
+			printf("[ERRO] Não foi possível inicializar a Thread da tarefa MatMult (C-Benchmark).\n");
+			return(0);
+		}
+	}
+
+	/*********************************************************
+	* Iniciando C-Benchmark -> Bsort
+	**********************************************************/
+	if((arrayThreadParams[2] = malloc(sizeof(*arrayThreadParams[2]))) == NULL)
+	{
+		printf("[ERRO] Não foi possivel criar os parametros da tarefa Bsort (C-Benchmark).\n\n");
+		return (-1);
+	}
+	else
+	{
+		arrayThreadParams[2]->idTask = 2;
+
+		// Inicializando as tarefas...
+		if(pthread_create(arrayThreads[2], 0, init_task_matmult, (void *)arrayThreadParams[2]))
+		{
+			printf("[ERRO] Não foi possível inicializar a Thread da tarefa Bsort (C-Benchmark).\n");
+			return(0);
+		}
+	}
 
 	while(!getchar()); // Aguardo o usuario apertar alguma tecla para finalizar o escalonamento...
 
