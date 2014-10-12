351bd0bd3cc4412e102daa4987525242ee8fcc71
nosnilwar <nosnilwar@gmail.com>
2014-10-12 14:50:55 -0400

    Alterado configurações das tarefas para realização de novos experimentos, baseando em novas saída da ferramenta do Valentin.

--- EXPERIMENTOS/Experimento_Final_15_Tasks_Valentin/script_experimento.sh
+++ EXPERIMENTOS/Experimento_Final_15_Tasks_Valentin/script_experimento.sh
@@ -20,7 +20,7 @@ printf "PROCESSANDO EXPERIMENTO \n"
 ARGV="1 0 0 0 0"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 01 -> RAW_MONITOR[X] - PC[ ] - QTD_PC[00] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_SEM_PC_01.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_SEM_PC_01.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -29,7 +29,7 @@ sleep 5
 ARGV="1 1 0 0 1"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 02 -> RAW_MONITOR[X] - PC[X] - QTD_PC[01] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_01_PC_02.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_01_PC_02.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -38,7 +38,7 @@ sleep 5
 ARGV="0 1 0 0 1"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 03 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[01] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_01_PC_03.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_01_PC_03.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -47,7 +47,7 @@ sleep 5
 ARGV="1 1 0 0 2"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 04 -> RAW_MONITOR[X] - PC[X] - QTD_PC[02] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_02_PC_04.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_02_PC_04.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -56,7 +56,7 @@ sleep 5
 ARGV="0 1 0 0 2"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 05 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[02] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_02_PC_05.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_02_PC_05.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -65,7 +65,7 @@ sleep 5
 ARGV="1 1 0 0 3"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 06 -> RAW_MONITOR[X] - PC[X] - QTD_PC[03] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_03_PC_06.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_03_PC_06.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -74,7 +74,7 @@ sleep 5
 ARGV="0 1 0 0 3"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 07 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[03] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_03_PC_07.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_03_PC_07.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -83,7 +83,7 @@ sleep 5
 ARGV="1 1 0 0 4"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 08 -> RAW_MONITOR[X] - PC[X] - QTD_PC[04] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_04_PC_08.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_04_PC_08.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -92,7 +92,7 @@ sleep 5
 ARGV="0 1 0 0 4"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 09 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[04] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_04_PC_09.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_04_PC_09.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -101,7 +101,7 @@ sleep 5
 ARGV="1 1 0 0 5"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 10 -> RAW_MONITOR[X] - PC[X] - QTD_PC[05] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_05_PC_10.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_05_PC_10.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -110,7 +110,7 @@ sleep 5
 ARGV="0 1 0 0 5"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 11 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[05] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_05_PC_11.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_05_PC_11.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -119,7 +119,7 @@ sleep 5
 ARGV="1 1 0 0 6"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 12 -> RAW_MONITOR[X] - PC[X] - QTD_PC[06] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_06_PC_12.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_06_PC_12.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -128,7 +128,7 @@ sleep 5
 ARGV="0 1 0 0 6"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 13 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[06] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_06_PC_13.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_06_PC_13.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -137,7 +137,7 @@ sleep 5
 ARGV="1 1 0 0 7"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 14 -> RAW_MONITOR[X] - PC[X] - QTD_PC[07] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_07_PC_14.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_07_PC_14.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -146,7 +146,7 @@ sleep 5
 ARGV="0 1 0 0 7"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 15 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[07] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_07_PC_15.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_07_PC_15.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -155,7 +155,7 @@ sleep 5
 ARGV="1 1 0 0 8"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 16 -> RAW_MONITOR[X] - PC[X] - QTD_PC[08] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_08_PC_16.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_08_PC_16.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -164,7 +164,7 @@ sleep 5
 ARGV="0 1 0 0 8"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 17 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[08] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_08_PC_17.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_08_PC_17.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -173,7 +173,7 @@ sleep 5
 ARGV="1 1 0 0 9"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 18 -> RAW_MONITOR[X] - PC[X] - QTD_PC[09] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_09_PC_18.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_09_PC_18.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -182,7 +182,7 @@ sleep 5
 ARGV="0 1 0 0 9"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 19 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[09] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_09_PC_19.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_09_PC_19.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -191,7 +191,7 @@ sleep 5
 ARGV="1 1 0 0 10"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 20 -> RAW_MONITOR[X] - PC[X] - QTD_PC[10] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_10_PC_20.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_10_PC_20.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -200,7 +200,7 @@ sleep 5
 ARGV="0 1 0 0 10"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 21 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[10] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_10_PC_21.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_10_PC_21.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -209,7 +209,7 @@ sleep 5
 ARGV="1 1 0 0 11"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 22 -> RAW_MONITOR[X] - PC[X] - QTD_PC[11] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_11_PC_22.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_11_PC_22.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -218,7 +218,7 @@ sleep 5
 ARGV="0 1 0 0 11"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 23 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[11] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_11_PC_23.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_11_PC_23.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -227,7 +227,7 @@ sleep 5
 ARGV="1 1 0 0 12"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 24 -> RAW_MONITOR[X] - PC[X] - QTD_PC[12] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_12_PC_24.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_12_PC_24.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -236,7 +236,7 @@ sleep 5
 ARGV="0 1 0 0 12"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 25 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[12] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_12_PC_25.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_12_PC_25.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -245,7 +245,7 @@ sleep 5
 ARGV="1 1 0 0 13"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 26 -> RAW_MONITOR[X] - PC[X] - QTD_PC[13] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_13_PC_26.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_13_PC_26.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -254,7 +254,7 @@ sleep 5
 ARGV="0 1 0 0 13"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 27 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[13] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_13_PC_27.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_13_PC_27.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -263,7 +263,7 @@ sleep 5
 ARGV="1 1 0 0 14"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 28 -> RAW_MONITOR[X] - PC[X] - QTD_PC[14] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_14_PC_28.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_14_PC_28.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -272,7 +272,7 @@ sleep 5
 ARGV="0 1 0 0 14"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 29 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[14] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_14_PC_29.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_14_PC_29.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -281,7 +281,7 @@ sleep 5
 ARGV="1 1 0 0 15"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 30 -> RAW_MONITOR[X] - PC[X] - QTD_PC[15] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_15_PC_30.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_15_PC_30.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -290,7 +290,7 @@ sleep 5
 ARGV="0 1 0 0 15"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 31 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[15] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_15_PC_31.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_15_PC_31.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -299,7 +299,7 @@ sleep 5
 ARGV="1 1 0 0 16"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 32 -> RAW_MONITOR[X] - PC[X] - QTD_PC[16] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_16_PC_32.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_16_PC_32.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -308,7 +308,7 @@ sleep 5
 ARGV="0 1 0 0 16"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 33 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[16] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_16_PC_33.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_16_PC_33.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -317,7 +317,7 @@ sleep 5
 ARGV="1 1 0 0 17"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 34 -> RAW_MONITOR[X] - PC[X] - QTD_PC[17] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_17_PC_34.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_17_PC_34.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -326,7 +326,7 @@ sleep 5
 ARGV="0 1 0 0 17"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 35 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[17] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_17_PC_35.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_17_PC_35.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -335,7 +335,7 @@ sleep 5
 ARGV="1 1 0 0 18"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 36 -> RAW_MONITOR[X] - PC[X] - QTD_PC[18] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_18_PC_36.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_18_PC_36.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -344,7 +344,7 @@ sleep 5
 ARGV="0 1 0 0 18"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 37 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[18] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_18_PC_37.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_18_PC_37.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -353,7 +353,7 @@ sleep 5
 ARGV="1 1 0 0 19"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 38 -> RAW_MONITOR[X] - PC[X] - QTD_PC[19] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_19_PC_38.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_19_PC_38.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -362,7 +362,7 @@ sleep 5
 ARGV="0 1 0 0 19"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 39 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[19] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_19_PC_39.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_19_PC_39.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -371,7 +371,7 @@ sleep 5
 ARGV="1 1 0 0 20"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 40 -> RAW_MONITOR[X] - PC[X] - QTD_PC[20] - Processando..."
-rtai-load > logs_finais/log_COM_MONITOR_COM_20_PC_40.txt
+rtai-load > logs_finais_2/log_COM_MONITOR_COM_20_PC_40.txt
 printf " - Concluíudo. \n"
 sleep 5
 
@@ -380,7 +380,7 @@ sleep 5
 ARGV="0 1 0 0 20"
 echo "$TARGET:lxrt:./$TARGET $ARGV;popall:Control+C para cancelar a execucao! ;)" > $RUNINFO
 printf "Experimento 41 -> RAW_MONITOR[ ] - PC[X] - QTD_PC[20] - Processando..."
-rtai-load > logs_finais/log_SEM_MONITOR_COM_20_PC_41.txt
+rtai-load > logs_finais_2/log_SEM_MONITOR_COM_20_PC_41.txt
 printf " - Concluíudo. \n"
 sleep 5
 
